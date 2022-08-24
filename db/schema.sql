35caa4c676b6458a5eee103ac2f654e5eb137e50
Ben Smerd <bsmerd@adam.com.au>
2022-08-24 21:37:22 +0930

    added in the schema for database and tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,31 @@
--- ADD IN THE COMMANDS THAT ARE BASICALLY GIVEN FROM THE MODULE PAGE ON BOOTCAMP DASHBOARD
---INCLUDE THE CREATE DATABASE ENTRIES
--- CREATE THE TABLSE AND ADD IN THEIR PROPERTIRES
+
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+USE employee_tracker;
+
+
+CREATE TABLE department (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT DEFAULT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
